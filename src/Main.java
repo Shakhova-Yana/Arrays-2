fb6f66aba310c533f10306809381cf2ae71cc1ec
НафНафДурикович <yana88yana99@gmail.com>
2024-04-23 23:12:51 +0300

    Задачи 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
+//Task1
+        System.out.println("\nTask 1");
+        int[] salaries = {15_000, 35_000, 50_000, 80_000};
+        int sunSalaries = 0;
+        for (int salary : salaries) {
+            sunSalaries += salary;
+        }
+        {
+            System.out.printf("Сумма трат за месяц составила %s рублей%n", sunSalaries);
+        }
+//Task2
+        System.out.println("\nTask 2");
+        int[] costsInWeeks = {10_000, 5_000, 15_000, 20_000};
 
+        int minSalary = costsInWeeks[0];
+        int maxSalary = costsInWeeks[0];
+
+        for (int i : costsInWeeks) {
+            if (i > maxSalary) {
+                maxSalary = i;
+            }
+        }
+        for (int i : costsInWeeks) {
+            if (i < minSalary) {
+                minSalary = i;
+            }
+        }
+
+        {
+            System.out.printf("Максимальная сумма трат за неделю составила %s рублей%n" +
+                    "Минимальная сумма трат за неделю составила %s рублей%n", maxSalary, minSalary);
+        }
+//Task3
+        System.out.println("\nTask 3");
     }
 }
